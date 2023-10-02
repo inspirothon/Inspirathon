27721fe04eaad861a68b5596ebd8fbad664d66aa
Isaraphil <118053290+meisara1ta@users.noreply.github.com>
2023-10-02 14:32:03 +0530

    first commit by israphil

---
+++ calculato.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/calci", function(req , res){
+    res.send("Hello Tamanna");
+});
+
+app.listen(3000, function(){
+    console.log("server started at port  no 3000");
+});
